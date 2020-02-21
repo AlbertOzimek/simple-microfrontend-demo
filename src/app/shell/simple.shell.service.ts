a1d03b6a4ae3381f92f62bbb5f66b1353f5ee4c3
Albert Ozimek <albert.ozimek@aerztekasse.ch>
2020-02-21 10:49:11 +0100

    eliminating errors for home

--- src/app/shell/simple.shell.service.ts
+++ src/app/shell/simple.shell.service.ts
@@ -4,18 +4,18 @@ import { SysEvent } from './sys-event';
 
 @Injectable({providedIn: 'root'})
 export class SimpleShellService {
-    
+
     frontends: { [name: string]: HTMLElement } = {};
 
     serviceBus = new Subject<SysEvent>();
 
     init() {
-        this._navigate(location.hash.substr(1));
+        // this._navigate(location.hash.substr(1));
 
         this.serviceBus.subscribe(msg => {
             console.debug('msg', msg);
         })
-        
+
         window['_serviceBus'] = this.serviceBus;
 
         this.load('client-a');
@@ -27,18 +27,18 @@ export class SimpleShellService {
 
         const outlet = document.getElementById('content');
         const bundle = `assets/micro-frontends/${clientName}/main.js`;
-    
+
         const script = document.createElement('script') as HTMLScriptElement;
         script.src = bundle;
         document.body.appendChild(script);
-    
+
         const rootElm = document.createElement(clientName);
         rootElm.hidden = true;
         outlet.appendChild(rootElm);
-    
+
         this.frontends[clientName] = rootElm;
     }
-  
+
     _navigate(path: string) {
         const segments = path.split('/');
         const clientName = segments.length >= 2 ? segments[1] : '';
@@ -46,20 +46,20 @@ export class SimpleShellService {
     }
 
     navigate(clientName: string, path: string) {
-      
+
       if (!this.frontends[clientName]) {
         this.load(clientName);
       }
-  
+
       for (const fe of Object.getOwnPropertyNames(this.frontends)) {
         const rootElm = this.frontends[fe];
         rootElm.hidden = true;
       }
 
       this.frontends[clientName].hidden = false;
-  
+
       location.hash = path;
-  
+
     }
-    
+
 }
